b4d0aaa85dbba55ffcbc79051860ec26cbc721e8
Michael E Fisher <fisher@streak.com>
2021-06-19 20:02:32 -0400

    fixup and make sure copy and sync work

--- drive.js
+++ drive.js
@@ -19,31 +19,54 @@ const to = process.env.REMOTE_TO;
 const dir = process.env.DIR;
 
 // Remote is in remote:dir format
-const copy_from = `${from}` + ':' + `${dir}`;
-const copy_to = `${to}` + '/' + `${dir}`;
+// TODO: This is just in Google Drive format
+const command_from = `${from}` + ':' + `${dir}`;
+// TODO: This is just in GCP format
+const command_to = `${to}` + '/' + `${dir}`;
 
-//TODO: Set drive-server-side-across-configs automatically if src and dest are drive
-//
+// TODO: Set drive-server-side-across-configs automatically
+// if src and dest are drive
+const commands = process.argv.slice(2);
+if (!commands.includes('sync') && !commands.includes('copy')) {
+  console.error('Right now, command must be sync or copy');
+  process.exit(1);
+}
 
+// TODO: This should all read from the CLI args
+// and take their own flags
 const args = [
 //  "--drive-server-side-across-configs",
 //  "--interactive",
+  "--dry-run",
   "--verbose",
   "--gcs-bucket-policy-only",
+  "--fast-list",
+  "--interactive",
 ]
 
-// A little gross, admittedly
-const ls = rclone.copy(
-  copy_from,
-  copy_to,
-  ...args
-);
+// Command is either () || null
+let command = null;
+if (commands[0] == "sync") {
+  command = rclone.sync(
+    command_from,
+    command_to,
+    ...args
+  );
+} else if (commands[0] == "copy") {
+  command = rclone.copy(
+    command_from,
+    command_to,
+    ...args
+  );
+}
 
-ls.stdout.on("data", (data) => {
-  console.log(data.toString());
-});
+if (command) {
+  command.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
 
-ls.stderr.on("data", (data) => {
-  console.error(data.toString());
-});
+  command.stderr.on("data", (data) => {
+    console.error(data.toString());
+  });
+}
 
